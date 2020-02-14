9a56ece17658482a483724ab77a6b96193ca52e0
SaiAditya <edesaiaditya@gmail.com>
2020-02-14 18:12:56 +0530

    [Kiran] Refactor:to return all the prime factors

--- PrimeFactor/pfBL.js
+++ PrimeFactor/pfBL.js
@@ -3,25 +3,22 @@ module.exports={
 let count=0;
 let a=[];
 let n=N;
-for(let i=2;i<=N;i++){
-
-
-    while(n%i==0){
-        count++;
+for(let i=2;i<=N;i++)
+    {
+       while(n%i==0)
+       {
+        //count++;
         n=n/i;
-        for(let j=0;j<count;j++){
-            a[j]=i;
-        }
+        a[count++]=i;
+       }
+
+   }
+if(n==1)
+    {
+     return a;
     }
 
-}
-
-return a;
-
-
     }
-
-
 
 
 }
