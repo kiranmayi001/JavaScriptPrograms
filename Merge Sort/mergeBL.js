0c5fa404e95633b47b2bda16c4894c3d0ab8b940
SaiAditya <edesaiaditya@gmail.com>
2020-02-14 11:50:12 +0530

    [Kiran] Refactor: chnaged the program for sorting an array

--- Merge Sort/mergeBL.js
+++ Merge Sort/mergeBL.js
@@ -1,25 +1,26 @@
-var a=[10,13,6,2,11,7];
-
-var low =0;
-var high =a.length-1;
-var mid;
-function host(a) {
-if(a.length == 1){
-console.log(a);
-return a;
+module.exports={
+  mergeSort:(a)=>{
+    if(a.length<2)
+    return a;
+let mid=Math.floor(a.length/2);
+let left=a.slice(0,mid);
+let right=a.slice(mid,a.length);
+return merge(mergeSort(left),mergeSort(right));
+},
+merge:(left,right)=>{
+let resultarr=[];
+while(left.length && right.length){
+  if(left[0]<right[0]){
+    resultarr.push(left.shift());
+  }
+  else {
+    resultarr.push(right.shift());
+  }
 }
-else{
-bla=mergesort(a,low,high);
-console.log(bla);
-return mergesort(a,low,high); //will returned a sorted array or list if length of the list is >1
-//function mergesort() is being called
-
+while(left.length)
+resultarr.push(left.shift());
+while(right.length)
+resultarr.push(right.shift());
+return resultarr;
 }
 }
-function mergesort(a,low,high) {
-if (a.length==1){
-console.log(a);
-return a;
-
-}
-else
--- Merge Sort/mergeMain.js
+++ Merge Sort/mergeMain.js
@@ -1,12 +1,13 @@
-et readline = require('readline-sync');
-let callFunction = require('./anagramBL');
+let readline = require('readline-sync');
+let callFunction = require('./mergeBL');
 try {
-    llet a=[];
+    let a=[];
     let n = readline.question('enter size of an array ');
-    for(var i=0;i<n;i++){
+    for(let i=0;i<n;i++){
     a[i]=readline.question('element is ');
-   
-    callFunction.host(a); //calling checkAnagram()
+    }
+    merge=callFunction.mergeSort(a); //calling merge to sort an unsorted array()
+    console.log(merge);
 }
 catch (err) {
     console.log(err);
