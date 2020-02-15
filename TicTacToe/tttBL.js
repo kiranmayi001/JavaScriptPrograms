dff3a274a74e14f64ba9d85ba5bb241c95f0d722
SaiAditya <edesaiaditya@gmail.com>
2020-02-15 11:48:03 +0530

    [Kiran] Refactor:made few changes using this kyword to acces class instances

--- TicTacToe/tttBL.js
+++ TicTacToe/tttBL.js
@@ -1,29 +1,136 @@
-/*****************************************************************************
- *  Execution       :   1. default node         cmd> node anagramMain.js
- *                      2. if nodemon installed cmd> nodemon anagramMain.js
- *
- *  Purpose         : Anagram Detection.
- *
- *  @description    : index of a 2 dimention array is given as an input to mark the "x" and a "o"
- *                    For Example: lets say "i" and "j" is 0 and 3 respectively it will mark "x" or "o" according to the turn. 
- *            
- *
- *  @file           : tttMain.js
- *  @overview       :It is a Tic Tac toe game 
- * 
- *  @module         : module_name - This is optional if expeclictly its an npm or local package
- *  @author         : Kiran Mayi <kiranmayi.raj29@gmail.com>
- *  @version        : v`1.11.1
- *  @since          : 14-02-2020
- ******************************************************************************/
+const readline=require('readline-sync');
+class TTT{
+    constructor(){
+this.isEmpty="true";
+this.player=0;
+this.board = [[]];
+    }
 
-let readline = require('readline-sync');
-let callFunction = require('./anagramBL');
-try {
-    let firstString = readline.question('enter first string ');
-    let secondString = readline.question('enter second string ');
-    callFunction.checkAnagram(firstString, secondString); //calling checkAnagram()
+hostplay(){
+    this.initboard();
+    while(this.isEmpty){
+    console.log("Player turn");
+        this.putVal();
+        this.displayboard();
+        if(this.win()){
+            console.log("Yay Player Won");
+            break;
+        }
+        this.player=1;
+        console.log("Player turn");
+        this.putVal();
+        this.displayboard();
+        if(this.win()){
+            console.log("Yay Computer Won");
+            break;
+        }
+        this.player=0;
+       }
+       console.log("Draw Match");
 }
-catch (err) {
-    console.log(err);
+
+
+
+initboard(){
+    
+// Loop to create 2D array using 1D array 
+for (let i = 0; i < 3; i++) { 
+this.board[i] =[[]]; 
 }
+  
+// Loop to initilize 2D array elements. 
+for (let i = 0; i < 3; i++) { 
+    for (let j = 0; j < 3; j++) { 
+        this.board[i][j] = -10; 
+  
+}
+}
+this.displayboard();
+}
+
+
+
+displayboard(){
+    let count=0;
+    for( let i=0;i<this.board.length;i++)
+    {
+
+        console.log("---------------");
+        console.log("||");
+        for(let j=0;j<this.board.length;j++){
+            if(this.board[i][j]==0 )
+            {
+                count++;
+                console.log(" O |");
+            }
+            else if(this.board[i][j]==1){
+                count++;
+                console.log(" X |");
+            }
+            else{
+                console.log("   |");
+            }
+        }
+    }
+    if(count == 9){
+        this.isempty="false";
+    }
+    console.log("---------------");
+}
+
+
+
+putVal(){
+    // to give the index values so as to mark the the value accordingly and display  it by the displayboard func; 
+    let i;
+    let j;
+    if(this.player%2==1){
+        i = parseInt ((Math.random() * 10) % 3);
+        j = parseInt ((Math.random() * 10) % 3);
+        console.log(i+" "+j);
+    }
+    else{
+
+    i=readline.questionInt();
+    j=readline.questionInt();
+    }
+    //checking if the board index is empty for the player or the computer to place the value
+    if(this.board[i][j]==-10){
+        if (this.player % 2 == 0) {
+            this.board[i][j] = 0;
+        } else if(this.player%2==1){
+        this.board[i][j]=1;
+            console.log("Computer Chooses " + i + " " + j);
+        }
+    }
+
+     else{
+        this.putVal();
+     }
+}
+
+
+
+
+win(){
+    return ((this.board[0][0] + this.board[0][1] + this.board[0][2] == this.player * 3)
+    || (this.board[1][0] + this.board[1][1] + this.board[1][2] == this.player * 3)
+    || (this.board[2][0] + this.board[2][1] + this.board[2][2] == this.player * 3)
+    || (this.board[0][0] + this.board[1][0] + this.board[2][0] == this.player * 3)
+    || (this.board[0][1] + this.board[1][1] + this.board[2][1] == this.player * 3)
+    || (this.board[0][2] + this.board[1][2] + this.board[2][2] == this.player * 3)
+    || (this.board[0][0] + this.board[1][1] + this.board[2][2] == this.player * 3)
+    || (this.board[2][0] + this.board[1][1] + this.board[0][2] == this.player * 3));
+}
+}
+
+
+
+
+
+module.exports = {
+    TTT //exporting the class it to the main
+
+}
+
+
--- TicTacToe/tttMain.js
+++ TicTacToe/tttMain.js
@@ -1,71 +1,78 @@
-class TTT{
-    constructor(){
-var isEmpty="true";
-let player=0;
-    }
 
-hostplay(){
-    this.initboard();
-    while(this.isEmpty){
-    console.log("Player turn");
-        this.putVal();
-        this.displayboard();
-        if(this.win()){
-            console.log("Yay Player Won");
-        }
-        this.player=1;
-        console.log("Player turn");
-        this.putVal();
-        this.displayboard();
-        if(this.win()){
-            console.log("Yay Player Won");
-        }
-        this.player=0;
-       }
+/*****************************************************************************
+ *  Execution       :   1. default node         cmd> node anagramMain.js
+ *                      2. if nodemon installed cmd> nodemon anagramMain.js
+ *
+ *  Purpose         : Anagram Detection.
+ *
+ *  @description    : index of a 2 dimention array is given as an input to mark the "x" and a "o"
+ *                    For Example: lets say "i" and "j" is 0 and 3 respectively it will mark "x" or "o" according to the turn. 
+ *            
+ *
+ *  @file           : tttMain.js
+ *  @overview       :It is a Tic Tac toe game 
+ * 
+ *  @module         : module_name - This is optional if expeclictly its an npm or local package
+ *  @author         : Kiran Mayi <kiranmayi.raj29@gmail.com>
+ *  @version        : v`1.11.1
+ *  @since          : 14-02-2020
+ ******************************************************************************/
+
+let readline = require('readline-sync');
+let callFunction = require('./tttBL');
+try {
+
+tictactoe=new callFunction.TTT(); //calling TTT() just the class and not the function yet to start with the game.
+tictactoe.hostplay(); //calling hostplay() function from the TTT() class to start with the game. 
+
+}
+catch (err) {
+    console.log(err);
 }
 
 
 
-initboard(){
-    // Create one dimensional array 
-    var gfg = new Array(2); 
-  
-document.write("Creating 2D array <br>"); 
-  
-// Loop to create 2D array using 1D array 
-for (let i = 0; i < gfg.length; i++) { 
-board[i] = new Array(2); 
-} 
-  
-var h = 0; 
-  
-// Loop to initilize 2D array elements. 
-for (let i = 0; i < 3 i++) { 
-    for (let j = 0; j < 3; j++) { 
-        board[i][j] = -10; 
-  
-}
-}
-}
-putVal(){
-    for(let i=0;i<board.length;i++){
-        for(let j=0;j<board.length;j++){
-            if(board[i][j]==0){
-
-            }
-        }
-
-    }
-
-
-
-}
-displayboard(){}
-win(){}
 
 
 
 
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
