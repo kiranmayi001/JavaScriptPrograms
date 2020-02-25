b36f043ef993549d6917776bbc71c25c5bd6e74e
kiranmayi001 <kiranmayi.raj29@gmail.com>
2020-02-25 19:28:36 +0530

    [Kiran] refactor inundation and comments to user

--- test/apptest.js
+++ test/apptest.js
@@ -1,8 +1,8 @@
-const assert=require('chai').assert;
-const app=require('../app');
-describe('Check UserName from app', () => {
-    it('should be equal to or greater than 3 char', () => {
-        
-     assert.equal(app.username(),'hello');
+const assert = require('chai').assert; //assert to determine the status of the failure
+const app = require('../app');
+describe('Check UserName from app', () => { //holds collection of test
+    it('should be equal to or greater than 3 char', () => { //it() is the test itself
+
+        assert.equal(app.username(), 'hello');
     })
-   })
+})
