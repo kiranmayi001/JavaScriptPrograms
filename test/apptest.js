a5b2e930d6057886565f05964582a24cd88e3512
kiranmayi001 <kiranmayi.raj29@gmail.com>
2020-02-25 17:20:39 +0530

    [Kiran] Added usernmae using mocha and chai

--- test/apptest.js
+++ test/apptest.js
@@ -3,6 +3,6 @@ const app=require('../app');
 describe('Check UserName from app', () => {
     it('should be equal to or greater than 3 char', () => {
         
-     assert.equal(app.'hello');
+     assert.equal(app.username(),'hello');
     })
    })
