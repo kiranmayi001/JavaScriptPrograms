44b437360574c5ac41c1c8fac8dee33b2318dc50
kiranmayi001 <kiranmayi.raj29@gmail.com>
2020-02-25 16:45:32 +0530

    [Kiran] Added testing username program using mocha and chai

---
+++ test/apptest.js
@@ -0,0 +1,8 @@
+const assert=require('chai').assert;
+const app=require('../app');
+describe('Check UserName from app', () => {
+    it('should be equal to or greater than 3 char', () => {
+        
+     assert.equal(app.'hello');
+    })
+   })
