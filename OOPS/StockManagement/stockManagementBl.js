83c24b6756fe616c9d50a154c7df1f445c190618
kiranmayi001 <kiranmayi.raj29@gmail.com>
2020-02-28 15:51:48 +0530

    [Kiran] refactor to add the stock price in total

--- OOPS/StockManagement/stockManagementBl.js
+++ OOPS/StockManagement/stockManagementBl.js
@@ -15,13 +15,13 @@ class StockManagement{
         this.stockDataObj={stock:[]};
         let JSONread=fs.readFileSync('StockManagement.json'); // just reads the json file 
         let JSONretrive=JSON.parse(JSONread); //retrive as obj from a json string
-        this.stockDataobj.stock=JSONretrive.stock;// pulling array of Stock from Json file and assigning it into the obj
+        this.stockDataObj.stock=JSONretrive.stock;// pulling array of Stock from Json file and assigning it into the obj
     }
     addnewStock(){
         try{
 let name=readline.question("Enter the name of the stock : ");
-let numberofStock=readline.question("Enter the number of the stocks presented : ");
-let price=readline.question("Enter the price of that stock : ");
+let numberofStock=readline.questionInt("Enter the number of the stocks presented : ");
+let price=readline.questionInt("Enter the price of that stock : ");
 let new_stock=new Stock(name,numberofStock,price);
 this.stockDataObj.stock.push(JSON.parse(JSON.stringify(new_stock)));
 fs.writeFileSync('StockManagement.json',JSON.stringify(this.stockDataObj));
@@ -31,9 +31,9 @@ fs.writeFileSync('StockManagement.json',JSON.stringify(this.stockDataObj));
         }
     }
     stockReport(){
-   
-for(let i=0;i<this.stockDataobj.stock.length;i++){
-  var total_stock_price= total_stock_price+this.stockDataObj[i].price;
+        var total_stock_price=0;
+for(let i=0;i<this.stockDataObj.stock.length;i++){
+  parseInt(total_stock_price= parseInt(total_stock_price+this.stockDataObj.stock[i].price));
 }
 console.log(total_stock_price);
     }
